32ab68a42664c38cc03b9581a8fb2c0879a70d42
Rich Howell <rich@codenameowl.com>
2025-10-27 14:25:48 +0000

    Create AppConfig.cs

---
+++ HL7TCPListener/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL7TCPListener
+{
+    public class AppConfig
+    {
+        public int Port { get; set; } = 4040;
+        public string FolderPath { get; set; } = "";        
+    }
+}
